1a08ecc59107ca8d6d00469a99d7d658f3b55288
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2026-06-10 23:28:11 +0000

    Primeira versao API

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
+
+MODEL_PATH = os.path.join("models", "model.joblib")
+METRICS_PATH = os.path.join("models", "metrics.json")
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return jsonify({"message": "Hello World, Flask API is running!"})
+
+@app.route('/health-check', methods=['GET'])
+def health():
+    return jsonify({"message": "OK"})
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    return jsonify({"message": "OK"})
 
 if __name__ == '__main__':
     app.run(debug=True)
